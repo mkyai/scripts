a9d0e54f5df58648dfd9e33d8c144a8a21c27d6b
lovneet <lovneet@radiansys.com>
2023-05-20 20:05:16 +0530

    test

--- test.sh
+++ test.sh
@@ -2,12 +2,13 @@
 
 sleep 10
 if curl --output /dev/null --silent --head --fail http://localhost:3000; then
-    msg='<!channel>\n[$(date +%T)] : Server is up :arrows_counterclockwise:\nDeployment successful.'
+    msg="<!channel>\n[$(date +%T)] : Server is up :white_check_mark:\nDeployment successful."
 else
-    msg='<!channel>\n[$(date +%T)] : Server is down :x:\nPlease review code or rollback to previous stable version.'
+    msg="<!channel>\n[$(date +%T)] : Server is down :x:\nPlease review code or rollback to previous stable version."
 fi
 
-[ -f ./slack.sh ] && ./slack.sh $msg || 
+[ -f ./slack.sh ] && ./slack.sh "$msg" || 
 (curl https://raw.githubusercontent.com/mkyai/scripts/master/slack.sh -o slack.sh && 
 chmod +x ./slack.sh && 
-./slack.sh $msg)
+./slack.sh "$msg")
+
