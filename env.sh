77ad541f55a6e088d589d914783470fed0a03f7d
lovneet <lovneet@radiansys.com>
2023-05-20 20:49:57 +0530

    env

--- env.sh
+++ env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd microservices
+for dir in microservices/*/
 
-for dir in */; 
 do
+  
   dir=${dir%*/}
-
   cp .env "$dir"/.env
   echo -e "\nMS=\"${dir#microservices/}\"" >> "${dir}/.env"
 
