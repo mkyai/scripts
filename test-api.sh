d6696eb89d6f4c882b3a0e828d270349108a498a
Lovneet <99083966+Lovneet-s@users.noreply.github.com>
2023-08-23 01:02:23 +0530

    Update test-api.sh

--- test-api.sh
+++ test-api.sh
@@ -7,7 +7,8 @@ else
     msg="[BE] Deployment failed :x:"
 fi
 
-[ -f ./slack.sh ] && ./slack.sh "$msg" || 
-(curl https://raw.githubusercontent.com/mkyai/scripts/master/slack.sh -o slack.sh && 
-chmod +x ./slack.sh && 
-./slack.sh "$msg")
+echo $msg > msg
+# [ -f ./slack.sh ] && ./slack.sh "$msg" || 
+# (curl https://raw.githubusercontent.com/mkyai/scripts/master/slack.sh -o slack.sh && 
+# chmod +x ./slack.sh && 
+# ./slack.sh "$msg")
