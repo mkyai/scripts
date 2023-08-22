de8083dbceaa4e301579fcff1bb2fc2754e8c3c4
Lovneet <99083966+Lovneet-s@users.noreply.github.com>
2023-08-22 20:46:49 +0530

    Update test-web.sh

--- test-web.sh
+++ test-web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 10
+sleep 2
 source .env
 if curl --output /dev/null --silent --head --fail "$WEB_URL" then
     msg="[FE] Deployment successful :white_check_mark:\n- $(git log -1 --pretty=%B)"
@@ -8,7 +8,8 @@ else
     msg="[FE] Deployment failed :x:"
 fi
 
-[ -f ./slack.sh ] && ./slack.sh "$msg" || 
-(curl https://raw.githubusercontent.com/mkyai/scripts/master/slack.sh -o slack.sh && 
-chmod +x ./slack.sh && 
-./slack.sh "$msg")
+echo $msg > msg
+#[ -f ./slack.sh ] && ./slack.sh "$msg" || 
+#(curl https://raw.githubusercontent.com/mkyai/scripts/master/slack.sh -o slack.sh && 
+#chmod +x ./slack.sh && 
+#./slack.sh "$msg")
