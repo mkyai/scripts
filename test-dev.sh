486500d08bde3062db7c242aea5b86bc55c7b2c7
Lovneet <99083966+Lovneet-s@users.noreply.github.com>
2024-03-02 14:57:30 +0530

    Create test-dev.sh

---
+++ test-dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 15
+if curl --output /dev/null --silent --head --fail http://localhost:3000; then
+    msg="[Dev] Deployment successful :white_check_mark:\n- $(git log -1 --pretty=%B)"
+else
+    msg="[Dev] Deployment failed :x:"
+fi
+
+[ -f ./slack.sh ] && ./slack.sh "$msg" || 
+(curl https://raw.githubusercontent.com/mkyai/scripts/master/slack.sh -o slack.sh && 
+chmod +x ./slack.sh && 
+./slack.sh "$msg")
